0cf3d524454bd4a8b73d68ffd2d8da7b65dae50e
icedlatter <s7201844@gmail.com>
2025-02-22 16:44:52 -0600

    hotfix comment out abs encoder

--- src/main/java/frc/robot/subsystems/CoralIntake.java
+++ src/main/java/frc/robot/subsystems/CoralIntake.java
@@ -31,7 +31,7 @@ public class CoralIntake extends SubsystemBase {
     private final SparkMaxConfig armMotorConfig = new SparkMaxConfig();
     private final SparkClosedLoopController armMotorController = armMotor.getClosedLoopController();
     private final RelativeEncoder armEncoder = armMotor.getEncoder();
-    private final AbsoluteEncoder absEncoder = armMotor.getAbsoluteEncoder();
+    //private final AbsoluteEncoder absEncoder = armMotor.getAbsoluteEncoder();
     
     //look at revlib for arm motor: getForwardLimitSwitch
     //look at revlib for intake motor: getOutputCurrent
@@ -40,8 +40,8 @@ public class CoralIntake extends SubsystemBase {
     private double velocity = 0.0;
     private boolean hasCoral = false;
 
-    private final SparkFlex rollerMotor = new SparkFlex(rollerMotorID, MotorType.kBrushless);
-    private final SparkFlexConfig rollerMotorConfig = new SparkFlexConfig();
+    private final SparkMax rollerMotor = new SparkMax(rollerMotorID, MotorType.kBrushless);
+    private final SparkMaxConfig rollerMotorConfig = new SparkMaxConfig();
     private final SparkClosedLoopController rollerMotorController = rollerMotor.getClosedLoopController();
     private final RelativeEncoder rollerEncoder = rollerMotor.getEncoder();
     
@@ -130,7 +130,6 @@ public class CoralIntake extends SubsystemBase {
     public void periodic() {
         // ill change this later
         armMotorController.setReference(setPoint*ARM_GEAR_RATIO, ControlType.kMAXMotionPositionControl);//MAXMotionPositionControl
-        //isAtSetPoint = getIsNearSetPoint();
         hasCoral = !IRsensor.get(); 
 
         SmartDashboard.putBoolean("is at setpoint",getIsNearSetPoint());
@@ -138,7 +137,7 @@ public class CoralIntake extends SubsystemBase {
         SmartDashboard.putNumber("arm encoder", armEncoder.getPosition());
         SmartDashboard.putNumber("roller amps", rollerMotor.getOutputCurrent());
         SmartDashboard.putBoolean("hasCoral", hasCoral);
-        SmartDashboard.putNumber("absEncoder", absEncoder.getPosition());
+        //SmartDashboard.putNumber("absEncoder", absEncoder.getPosition());
         
 
         
